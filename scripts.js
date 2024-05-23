0622e8cce18a78088dc517bba480ec404fa0d90e
Katharine Holland <146892906+KHolland686@users.noreply.github.com>
2024-05-23 11:16:24 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+// Smooth scroll to anchor links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
